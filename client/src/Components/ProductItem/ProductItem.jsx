b366703e42d506a3a8efa8224b488c867513d816
tiennehihi <vuductientienti@gmail.com>
2024-02-23 23:43:17 +0700

    update

--- client/src/Components/ProductItem/ProductItem.jsx
+++ client/src/Components/ProductItem/ProductItem.jsx
@@ -1,76 +1,35 @@
-import './style.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './style.css'
 
 function ProductItem({ product }) {
     const { name, category, price, image } = product;
+    // const name = product.name;
+    // const category = product.category
+    // const price = product.price
+    // const image = product.image
+
     return (
-        <>
-            <div className="">
-                <a href='#' className='item-product'>
-                    <div className="item-product-img" style={{backgroundImage: `url(${image})`}}></div>
-                    <div className="name-rate-sold">
-                        <h3 className="item-product-name">{name}</h3>
-                        {/* <div className="rate-and-sold flex justify-start items-baseline">
-                            <div className="item-product-rating">
-                                {[...Array(5).keys()].map(index => (
-                                    <i key={index} className={`fa-solid fa-star ${index < rating ? 'star-gold' : 'star-gray'}`}></i>
-                                ))}
-                            </div>
-                            <div className="item-product-sold">Đã bán {sold}</div>
-                        </div> */}
+        <div className="w-20 col-md-3 col-sm-4 col-6 mb-2">
+            <div className="product__item">
+                <a to='#' className='product__item-link'>
+                    <div className="product__item-image">
+                        <img src={image} alt={name} />
                     </div>
-                    <div className="item-product-price">
-                        <span className="item-product-price-current">{price}<sup>đ</sup></span>
-                        {/* <span className="item-product-price-discount">-{discount}%</span> */}
+                    <div className="product__item-info">
+                        <div className="nameAndCategory">
+                            <h3 className="product__item-name">{name}</h3>
+                            <div className="product__item-category">Thể loại: {category}</div>
+                        </div>
+                        <div className="product__item-price">
+                            <span className="product__item-price-current">{price}<sup>đ</sup></span>
+                        </div>
                     </div>
-                    {/* <div className="item-product-delivery">{delivery}</div> */}
-                    <div className="item-product-categories">{category}</div>
-                    <button className='addToCart'>Add to cart</button>
                 </a>
+                <button className='product__item-button'>Add to cart</button>
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
 export default ProductItem;
-
-// import './style.css';
-// import React from 'react';
-// // import { Link } from 'react-router-dom';
-
-// function ProductItem({ product }) {
-//     const { name, rating_average, list_price } = product;
-//     const base_url = product.images[0]?.base_url; // Truy cập vào base_url trong object con images[0]
-//     const quantitySoldText = product.quantity_sold?.text
-//     const name_categorie = product.categories?.name
-//     const discount = Math.floor(Math.random() * 100) + 1
-//     return (
-//         <>
-//             <div className="row">
-//                 <div className='product__item'>
-//                     <div className="product__item-img" style={{backgroundImage: `url(${base_url})`}}></div>
-//                     <div className="name-rate-sold">
-//                         <h3 className="product__item-name">{name}</h3>
-//                         <div className="rate-and-sold">
-//                             <div className="product__item-rating">
-//                                 {[...Array(5).keys()].map(index => (
-//                                     <i key={index} className={`fa-solid fa-star ${index < rating_average ? 'star-gold' : 'star-gray'}`}></i>
-//                                 ))}
-//                             </div>
-//                             <div className="product__item-sold">{quantitySoldText}</div>
-//                         </div>
-//                     </div>
-//                     <div className="product__item-price">
-//                         <span className="product__item-price-current">{list_price}<sup>đ</sup></span>
-//                         <span className="product__item-price-discount">-{discount}%</span>
-//                     </div>
-//                     {/* <div className="product__item-delivery text-center mt-6 pt-2">{delivery}</div> */}
-//                     <div className="product__item-categories">{name_categorie}</div>
-//                 </div>
-//             </div>
-//         </>
-//     )
-// }
-
-// export default ProductItem;
--- client/src/Components/ProductList/ProductList.jsx
+++ client/src/Components/ProductList/ProductList.jsx
@@ -3,50 +3,17 @@ import ProductItem from '../ProductItem/ProductItem';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function ProductList() {
+function ProductList({ searchTerm}) {
   const [productList, setProductList] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const productsPerPage = 10; // Số lượng sản phẩm trên mỗi trang
-  const products = [
-    {
-      "category": "Sách tiếng Việt",
-      "description": "Chúng ta đang sống trong một thời đại vội vã, dường như mọi giao tiếp đều ở trên màn hình xanh, và dường như chúng ta thường quên đi những niềm vui giản dị, những ấm áp yêu thương, những cử chỉ chân t...",
-      "id": 1,
-      "image": "https://salt.tikicdn.com/ts/product/82/82/80/c542073200c6e95d3e89840b2f41ce2e.jpg",
-      "name": "101 BÍ KÍP SỐNG SÓT NƠI CÔNG SỞ-Sửa lần 6",
-      "price": 138000
-    },
-    {
-      "category": "Sách tư duy ",
-      "description": "***Nội dung phiên bản tái bản không có thay đổi so với phiên bản đầu, chỉ thay đổi format và số đăng ký năm theo Luật Xuất Bản ***---------------------------------------Bạn có biết:- Các thủ khoa ...",
-      "id": 2,
-      "image": "https://salt.tikicdn.com/ts/product/29/c6/6d/64243ee01d5a0786fdb3c3977ec9c63e.jpg",
-      "name": "Chó sủa nhầm cây - Tại sao những gì ta biết về thành công có khi lại sai",
-      "price": 150000
-    },
-    {
-      "category": "Sách doanh nhân",
-      "description": "Sáng Tạo Không Giới Hạn Trong Kinh Doanh “…Đôi khi, thành công phụ thuộc vào một cái gì đó bên trong mỗi chúng ta, không phải là kết quả của việc nhiều tiền bạc, khả năng hô hào hay bằng cấp…” Tác...",
-      "id": 3,
-      "image": "https://salt.tikicdn.com/ts/product/93/28/53/c6b3299850c4af077f36ed73189620ad.jpg",
-      "name": "Sáng Tạo Không Giới Hạn Trong Kinh Doanh\"",
-      "price": 96000
-    },
-    {
-      "category": "Sách tiếng Việt",
-      "description": "“Vị chua chát của cái nghèo hòa trộn với vị ngọt ngào khi khám phá ra những điều khiến cuộc đời này đáng số một tác phẩm kinh điển của Brazil.”- Booklist“Một cách nhìn cuộc sống gần như hoàn chỉnh t...",
-      "id": 4,
-      "image": "https://salt.tikicdn.com/ts/product/5e/18/24/2a6154ba08df6ce6161c13f4303fa19e.jpg",
-      "name": "Cây Cam Ngọt Của Tôi",
-      "price": 108000
-    }
-  ]
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://86yfl7-8080.csb.app/books');
-        setProductList(response.data);
+        const response = await axios.get('http://127.0.0.1:5000/sanpham');
+        setProductList(response.data.products); // Lấy mảng sản phẩm từ thuộc tính products
+        // console.log(response.data.products)
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -54,11 +21,25 @@ function ProductList() {
 
     fetchData();
   }, []);
+  // console.log(typeof productList);
+  
+  
+  // Lọc danh sách sản phẩm dựa trên giá trị nhập vào từ ô tìm kiếm
+  // const filteredProducts = productList && productList.products ? Object.values(productList.products).filter(product =>
+  //   product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  // ) : [];
+  // console.log(filteredProducts);
+  const filteredProducts = productList.filter(product =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   // Tính toán chỉ mục của sản phẩm đầu tiên và cuối cùng trên trang hiện tại
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = productList.slice(indexOfFirstProduct, indexOfLastProduct);
+  // Kiểm tra xem productList có tồn tại và không rỗng trước khi sử dụng thuộc tính length
+  // const currentProducts = Array.isArray(productList) ? productList.slice(indexOfFirstProduct, indexOfLastProduct) : [];
+  const currentProducts = filteredProducts.slice(indexOfFirstProduct, indexOfLastProduct);
+  console.log(productList);
 
   // Chuyển đến trang tiếp theo
   const nextPage = () => {
@@ -71,15 +52,17 @@ function ProductList() {
   };
 
   return (
-    <div className="product-container">
+    <div className="product-container col-12">
       <div className="product-list">
-        {products.map((product, index) => (
-          <ProductItem key={index} product={product} />
-        ))}
+        <div className="row">
+          {currentProducts && currentProducts.map((product, index) => (
+            <ProductItem key={index} product={product} />
+          ))}
+        </div>
       </div>
 
       <div className="pagination">
-        <ul className="product-list__pagination">
+        <ul className="product-list__pagination d-flex">
           <li className="pagination-item">
             <button
               className="pagination-item__link"
@@ -90,7 +73,7 @@ function ProductList() {
             </button>
           </li>
           {/* Hiển thị số trang */}
-          {Array.from({ length: Math.ceil(productList.length / productsPerPage) }, (_, i) => (
+          {productList && Array.from({ length: Math.ceil(productList.length / productsPerPage) }, (_, i) => (
             <li key={i} className={`pagination-item ${currentPage === i + 1 ? 'active' : ''}`}>
               <button
                 className="pagination-item__link"
@@ -103,7 +86,7 @@ function ProductList() {
           <li className="pagination-item">
             <button
               className="pagination-item__link"
-              disabled={currentPage === Math.ceil(productList.length / productsPerPage)}
+              disabled={currentPage === Math.ceil((productList && productList.length) / productsPerPage)}
               onClick={nextPage}
             >
               <i className="pagination-item__icon fa-solid fa-angle-right"></i>
